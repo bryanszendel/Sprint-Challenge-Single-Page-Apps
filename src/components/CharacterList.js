90f65db115d7edf35200884137fab0b23d063e71
Bryan Szendel <bryan.szendel@gmail.com>
2019-07-26 10:48:23 -0600

    Part 1 Completed - character, location, and episode components created

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,41 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Container, Card, Icon, Image } from 'semantic-ui-react'
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
-
+  const [charData, setCharData] = useState([])
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, [])
-
-  return <section className='character-list grid-view'>
-
+    axios
+      .get('https://rickandmortyapi.com/api/character/?page=1')
+      .then(response => {
+        setCharData(response.data.results)
+    })
+    .catch(error => {
+      console.log('ERROR', error)
+    })
+  }, [charData])
+  console.log(charData)
+  return (
+    <section className='character-list grid-view'>
       <h2>TODO: `array.map()` over your state here!</h2>
+          <Container style={{display: "flex", flexDirection: "row", flexWrap: "wrap", justifyContent: "center"}}>
+            {charData.map((char, index) => {
+              return (
+                  <Card style={{width: "20%"}} key={index}>
+                    <Image src={char.image} />
+                    <Card.Content>
+                      <Card.Header>{char.name}</Card.Header>
+                      <Card.Meta>Species: {char.species}</Card.Meta>
+                      <Card.Description>Location: {char.location.name}</Card.Description>
+                      <Card.Description>Origin: {char.origin.name}</Card.Description>
+                    </Card.Content>
+                  </Card>
+              )
+            })}
+          </Container>
     </section>
-
+  )
 }
--- src/components/EpisodeList.js
+++ src/components/EpisodeList.js
@@ -1,35 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios'
-import { Container, Card, Image } from 'semantic-ui-react'
+import { Container, Card } from 'semantic-ui-react'
 
-export default function LocationList() {
+export function EpisodeList() {
   // TODO: Add useState to track data from useEffect
-  const [locationData, setLocationData] = useState([])
+  const [episodeData, setEpisodeData] = useState([])
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
     axios
-      .get('https://rickandmortyapi.com/api/location/')
+      .get('https://rickandmortyapi.com/api/episode/')
       .then(response => {
-        setLocationData(response.data.results)
+        setEpisodeData(response.results)
     })
     .catch(error => {
       console.log('ERROR', error)
     })
-  }, [locationData])
-  console.log(locationData)
+  }, [episodeData])
+  console.log(episodeData)
   return (
     <section className='character-list grid-view'>
-      <h2>LOCATIONSSS</h2>
+      <h2>EPISODESSS</h2>
           <Container style={{display: "flex", flexDirection: "row", flexWrap: "wrap", justifyContent: "center"}}>
-            {locationData.map((location, index) => {
+            {episodeData.map((episode, index) => {
               return (
                   <Card style={{width: "20%"}} key={index}>
-                    {/* <Image src={location.image} /> */}
                     <Card.Content>
-                      <Card.Header>{location.name}</Card.Header>
-                      <Card.Meta>Location Type: {location.type}</Card.Meta>
-                      <Card.Description>Dimensions: {location.dimension}</Card.Description>
+                      <Card.Header>{episode.name}</Card.Header>
+                      <Card.Meta>Species: {episode.species}</Card.Meta>
+                      <Card.Description>Location: {episode.name}</Card.Description>
+                      <Card.Description>Origin: {episode.name}</Card.Description>
                     </Card.Content>
                   </Card>
               )
